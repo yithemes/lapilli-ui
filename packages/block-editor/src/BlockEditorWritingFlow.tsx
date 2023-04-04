27938d8f0e012a0a4d44b1ac3c46e679cfebe840
Leanza Francesco <leanzafrancesco@gmail.com>
2023-04-04 15:38:48 +0200

    inspector wip

--- packages/block-editor/src/BlockEditorWritingFlow.tsx
+++ packages/block-editor/src/BlockEditorWritingFlow.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
 	BlockList,
 	// @ts-ignore No types for this exist yet.
 	BlockTools,
+	BlockInspector,
 	ObserveTyping,
 	WritingFlow,
+	// @ts-ignore No types for this exist yet.
+	__unstableBlockToolbarLastItem as BlockToolbarLastItem,
 	store as blockEditorStore
 } from '@wordpress/block-editor';
 import { useSelect, useDispatch } from "@wordpress/data";
+import { Button } from '@wordpress/components';
 
 // @ts-ignore
 import { styled } from "@yith/styles";
@@ -21,6 +25,30 @@ type BlockEditorWritingFlowProps = {
 	placeholder?: string;
 };
 
+const BlockEditorWritingFlowWrapper = styled( 'div', { name: 'BlockEditorWritingFlow', slot: 'Wrapper' } )( ( { theme } ) => ( {
+	display: 'flex',
+	'.block-editor-writing-flow': {
+		flex: 1,
+	}, '.block-editor-block-inspector': {
+		width: 280,
+		borderLeftStyle: 'solid',
+		borderLeftWidth: '1px',
+		borderLeftColor: theme.fields.borderColor,
+	},
+} ) );
+
+const BlockEditorToolbarActionsRoot = styled( 'div', { name: 'BlockEditorToolbarActions', slot: 'Root' } )( ( { theme } ) => ( {
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'flex-end',
+	flex: 1,
+	'.components-button.has-icon.has-icon': {
+		minWidth: '36px',
+		paddingLeft: '6px',
+		paddingRight: '6px',
+	}
+} ) );
+
 export default function BlockEditorWritingFlow(
 	{
 		blocks,
@@ -31,6 +59,7 @@ export default function BlockEditorWritingFlow(
 	const firstBlock = blocks[ 0 ];
 	const isEmpty = !blocks.length;
 	const { insertBlock, selectBlock } = useDispatch( blockEditorStore );
+	const [ isInspectorOpen, setIsInspectorOpen ] = useState( false );
 
 	const { selectedBlockClientIds } = useSelect( ( select ) => {
 		// @ts-ignore This selector is available in the block editor data store.
@@ -62,10 +91,25 @@ export default function BlockEditorWritingFlow(
 	}, [ isEmpty ] );
 
 	return <BlockTools>
-		<WritingFlow>
-			<ObserveTyping>
-				<BlockList/>
-			</ObserveTyping>
-		</WritingFlow>
+		<BlockEditorWritingFlowWrapper>
+			<WritingFlow>
+				<ObserveTyping>
+					<BlockList/>
+				</ObserveTyping>
+			</WritingFlow>
+			{
+				isInspectorOpen &&
+				<form onSubmit={ ( event ) => event.preventDefault() }>
+					<BlockInspector/>
+				</form>
+			}
+		</BlockEditorWritingFlowWrapper>
+
+		<BlockToolbarLastItem>
+			<BlockEditorToolbarActionsRoot>
+				<Button icon='admin-generic' label='Settings' onClick={ () => setIsInspectorOpen( _ => !_ ) } isPressed={ isInspectorOpen }/>
+			</BlockEditorToolbarActionsRoot>
+		</BlockToolbarLastItem>
+
 	</BlockTools>
 }
