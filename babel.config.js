79dd23644927e86619dfcadab1e515a2097ea99a
Leanza Francesco <leanzafrancesco@gmail.com>
2023-08-09 15:28:32 +0200

    fixed babel config to convert JSX into standard JS for builds

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = ( api ) => {
 	api.cache( true );
 
 	return {
-		presets: ['@babel/preset-typescript'],
-		plugins: ['@emotion/babel-plugin']
+		presets: ['@babel/preset-env','@babel/preset-typescript'],
+		plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-transform-react-jsx','@emotion/babel-plugin']
 	};
 };
