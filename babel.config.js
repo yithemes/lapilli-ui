18e13f9a6d26e47ae77d412e85d0d1f5433a3918
Leanza Francesco <leanzafrancesco@gmail.com>
2023-08-09 16:37:41 +0200

    trying tree shaking

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,6 @@ module.exports = ( api ) => {
 
 	return {
 		presets: ['@babel/preset-env','@babel/preset-typescript'],
-		plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-transform-react-jsx','@emotion/babel-plugin']
+		plugins: ['@babel/plugin-transform-react-jsx','@emotion/babel-plugin']
 	};
 };
